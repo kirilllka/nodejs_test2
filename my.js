879429b80b27d2e55506ad21c837dda4c5e904d2
Kirill Myakishev <kirillmyakishev@Kirills-MacBook-Pro.local>
2020-03-12 23:01:40 +0500

    moved to any ip

--- my.js
+++ my.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.2';
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World hohoho');
 });
 
 server.listen(port, hostname, () => {
