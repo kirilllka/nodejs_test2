48a26361ba0be5587f63b2943fc502b39093c487
Kirill Myakishev <kirillmyakishev@Kirills-MacBook-Pro.local>
2020-03-12 23:23:32 +0500

    using modules

--- my.js
+++ my.js
@@ -1,8 +1,12 @@
 var http = require('http');
 
+var dt = require('./mymodule1');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.write('Hello World!');
+  res.write('Now is: ' + dt.myDateTime());
+  res.end();
 }).listen(3000);
 
 console.log(`Server running...`);
