8f7475087ad5e19ff95ff03e62c85d6ba5caf992
Kirill Myakishev <kirillmyakishev@Kirills-MacBook-Pro.local>
2020-03-12 23:27:44 +0500

    trying req

--- my.js
+++ my.js
@@ -6,6 +6,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
   res.write('Now is: ' + dt.myDateTime());
+  res.write(req.url);
   res.end();
 }).listen(3000);
 
