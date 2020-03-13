0598f1b9537c4722f5ebd32511ec7c834cc42ae4
Kirill Myakishev <kirillmyakishev@Kirills-MacBook-Pro.local>
2020-03-13 16:09:27 +0500

    qqq

--- my.js
+++ my.js
@@ -19,7 +19,7 @@ http.createServer(function (req, res) {
 
   console.log('Answering: ' + q.a + " " + q.b + " to " + req.connection.remoteAddress);
 
-fs.readFile('./my.html', function(err, data) {
+fs.readFile('my.html', function(err, data) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(data);
   res.end();
